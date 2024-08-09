b597d2568ecc615135db341ab09586695d984d9e
1999azzar <azzar.mr.zs@gmail.com>
2024-08-09 21:25:30 +0700

    make it modular

---
+++ track.h
@@ -0,0 +1,10 @@
+#ifndef TRACK_H
+#define TRACK_H
+
+const char* trackNames[] = {
+  "001.wav", "002.wav", "003.wav", "004.wav", "005.wav",
+  // ... add remaining track names here
+  "286.wav"
+};
+
+#endif // TRACK_H
